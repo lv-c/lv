26458f93f7dcbce7bd99d5160ecb5c44b7e17494
lv <lv@54bad739-14b1-1b4c-9595-0bb2987a2747>
2010-01-03 15:26:44 +0000

    加入 Net.hpp
    git-svn-id: svn://23.27.117.66/h2/trunk@39 54bad739-14b1-1b4c-9595-0bb2987a2747

---
+++ lv/lv/FrameWork/Net.hpp
@@ -0,0 +1,19 @@
+// *********************************************************************
+//  Net   version:  1.0   ¡¤  date: 01/03/2010
+//  --------------------------------------------------------------------
+//  
+//  --------------------------------------------------------------------
+//  Copyright (C) jcfly(lv.jcfly@gmail.com) 2010 - All Rights Reserved
+// *********************************************************************
+// 
+// *********************************************************************
+
+#ifndef LV_NET_HPP
+#define LV_NET_HPP
+
+#include <lv/FrameWork/Net/Fwd.hpp>
+#include <lv/FrameWork/Net/TcpSession.hpp>
+#include <lv/FrameWork/Net/FlowSession.hpp>
+#include <lv/FrameWork/Net/ServerBase.hpp>
+
+#endif
