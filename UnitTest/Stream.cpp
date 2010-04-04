c50a9265a5e6d83537e0042ec4a2f10ae2063649
lv <lv@54bad739-14b1-1b4c-9595-0bb2987a2747>
2010-04-04 04:39:09 +0000

    上次没有提交完全。。
    git-svn-id: svn://23.27.117.66/h2/trunk@48 54bad739-14b1-1b4c-9595-0bb2987a2747

---
+++ UnitTest/Stream.cpp
@@ -0,0 +1,45 @@
+// *********************************************************************
+//  Stream   version:  1.0   ¡¤  date: 04/02/2010
+//  --------------------------------------------------------------------
+//  
+//  --------------------------------------------------------------------
+//  Copyright (C) jcfly(lv.jcfly@gmail.com) 2010 - All Rights Reserved
+// *********************************************************************
+// 
+// *********************************************************************
+
+#include "UnitTest.hpp"
+
+#include <lv/Stream/StreamBase.hpp>
+#include <lv/Stream/IBufferStream.hpp>
+#include <lv/Stream/OBufferStream.hpp>
+
+using namespace lv;
+
+BOOST_AUTO_TEST_CASE(test_stream_base)
+{
+	StreamBase stream_base;
+}
+
+
+BOOST_AUTO_TEST_CASE(test_stream)
+{
+	Buffer buf;
+
+	OBufferStream os(buf);
+
+	char str[] = "hello world";
+	size_t const size = sizeof(str);
+	os.write(str, size);
+
+	BOOST_CHECK_EQUAL(buf.size(), size);
+	BOOST_CHECK_EQUAL(memcmp(str, &buf[0], size), 0);
+
+	IBufferStream is(buf);
+	char new_str[size];
+	is.read(new_str, size);
+
+	BOOST_CHECK_EQUAL(memcmp(str, new_str, size), 0);
+
+	BOOST_CHECK_THROW(is.read(new_str, 1), std::ios_base::failure);
+}
