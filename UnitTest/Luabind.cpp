a46c087a66636ed9f1bd1962401b649294c6c9e0
lv <lv@54bad739-14b1-1b4c-9595-0bb2987a2747>
2012-09-27 08:17:48 +0000

    加上 lv::lua::extract
    git-svn-id: svn://23.27.117.66/h2/trunk@144 54bad739-14b1-1b4c-9595-0bb2987a2747

--- UnitTest/Luabind.cpp
+++ UnitTest/Luabind.cpp
@@ -12,6 +12,10 @@
 
 #include <lv/Luabind/Vector.hpp>
 #include <lv/Lua/Exec.hpp>
+#include <lv/Lua/Extract.hpp>
+#include <lv/LuaArchive/Vector.hpp>
+#include <lv/Serialization/Graphics/Point.hpp>
+#include <lv/Graphics/Stream/Point.hpp>
 
 #include <lua.hpp>
 
@@ -24,13 +28,26 @@ BOOST_AUTO_TEST_CASE(test_luabind)
 
 	lv::lua::bind_vector<int>(state, "IntVec");
 
+	// all global values
 	char const * script = 
-		"local v = IntVec()\n"
+		"v = IntVec()\n"
 		"v:push_back(1)\n"
-		"local i = v:at(0)\n"
+		"i = v:at(0)\n"
+		"pt = { 10, 20 }\n"
 		;
 
 	lv::lua::dostr(state, script);
+
+	luabind::object global = luabind::globals(state);
+
+	std::vector<int> vec = lv::lua::extract<std::vector<int> >(global["v"]);
+
+	BOOST_CHECK_EQUAL(vec.size(), 1);
+	BOOST_CHECK_EQUAL(vec[0], 1);
+
+	BOOST_CHECK_EQUAL(lv::lua::extract<int>(global["i"]), 1);
+
+	BOOST_CHECK_EQUAL(lv::lua::extract<lv::Point>(global["pt"]), lv::Point(10, 20));
 
 	lua_close(state);
 }
---
+++ lv/lv/Lua/Extract.hpp
@@ -0,0 +1,40 @@
+// *********************************************************************
+//  Extract   version:  1.0   ¡¤  date: 2012/09/27
+//  --------------------------------------------------------------------
+//  
+//  --------------------------------------------------------------------
+//  Copyright (C) jcfly(lv.jcfly@gmail.com) 2012 - All Rights Reserved
+// *********************************************************************
+// 
+// *********************************************************************
+
+#ifndef LV_LUA_EXTRACT_HPP
+#define LV_LUA_EXTRACT_HPP
+
+#include <lv/LuaArchive/LuaIArchive.hpp>
+
+#include <luabind/object.hpp>
+
+namespace lv { namespace lua {
+
+	template<class T>
+	T	extract(luabind::object const & obj)
+	{
+		if(luabind::type(obj) != LUA_TTABLE)
+		{
+			return luabind::object_cast<T>(obj);
+		}
+		else
+		{
+			T t;
+
+			LuaIArchive ia(obj);
+			ia >> t;
+
+			return t;
+		}
+	}
+
+} }
+
+#endif
