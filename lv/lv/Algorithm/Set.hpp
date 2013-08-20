14c52749aa6ded44a7847ecf613bdfcc97849699
lv <lv@54bad739-14b1-1b4c-9595-0bb2987a2747>
2013-08-20 14:41:21 +0000

    加入 Algorithm/Set.hpp
    git-svn-id: svn://23.27.117.66/h2/trunk@179 54bad739-14b1-1b4c-9595-0bb2987a2747

---
+++ lv/lv/Algorithm/Set.hpp
@@ -0,0 +1,86 @@
+// *********************************************************************
+//  Set   version:  1.0   ¡¤  date: 2013/08/20
+//  --------------------------------------------------------------------
+//  
+//  --------------------------------------------------------------------
+//  Copyright (C) jcfly(lv.jcfly@gmail.com) 2013 - All Rights Reserved
+// *********************************************************************
+// 
+// *********************************************************************
+
+#ifndef LV_ALGORITHM_SET_HPP
+#define LV_ALGORITHM_SET_HPP
+
+#include <set>
+#include <algorithm>
+
+namespace lv
+{
+
+#define STD_SET_OPERATION(op)	std:: ## op (lhs.begin(), lhs.end(), rhs.begin(), rhs.end(), std::inserter(ret, ret.begin()))
+
+	// difference
+
+	template<typename T, typename Pr, typename Ax>
+	std::set<T, Pr, Ax> & operator -= (std::set<T, Pr, Ax> & lhs, std::set<T, Pr, Ax> const & rhs)
+	{
+		std::set<T, Pr, Ax> ret;
+		STD_SET_OPERATION(set_difference)
+
+		lhs.swap(ret);
+		return lhs;
+	}
+
+	template<typename T, typename Pr, typename Ax>
+	std::set<T, Pr, Ax> operator - (std::set<T, Pr, Ax> const & lhs, std::set<T, Pr, Ax> const & rhs)
+	{
+		std::set<T, Pr, Ax> ret;
+		STD_SET_OPERATION(set_difference)
+
+		return ret;
+	}
+
+	// join
+
+	template<typename T, typename Pr, typename Ax>
+	std::set<T, Pr, Ax> & operator += (std::set<T, Pr, Ax> & lhs, std::set<T, Pr, Ax> const & rhs)
+	{
+		lhs.insert(rhs.begin(), rhs.end());
+		return lhs;
+	}
+
+	template<typename T, typename Pr, typename Ax>
+	std::set<T, Pr, Ax> operator + (std::set<T, Pr, Ax> const & lhs, std::set<T, Pr, Ax> const & rhs)
+	{
+		std::set<T, Pr, Ax> ret = lhs;
+
+		ret.insert(rhs.begin(), rhs.end());
+		return ret;
+	}
+
+	// intersect
+
+	template<typename T, typename Pr, typename Ax>
+	std::set<T, Pr, Ax> & operator &= (std::set<T, Pr, Ax> & lhs, std::set<T, Pr, Ax> const & rhs)
+	{
+		std::set<T, Pr, Ax> ret;
+		STD_SET_OPERATION(set_intersection);
+
+		lhs.swap(ret);
+		return lhs;
+	}
+
+	template<typename T, typename Pr, typename Ax>
+	std::set<T, Pr, Ax> operator & (std::set<T, Pr, Ax> const & lhs, std::set<T, Pr, Ax> const & rhs)
+	{
+		std::set<T, Pr, Ax> ret;
+		STD_SET_OPERATION(set_intersection);
+
+		return ret;
+	}
+
+#undef STD_SET_OPERATION
+
+}
+
+#endif
