0dce8d7ff74c42dfda6343626d8ea9122d200a85
lv <lv.jcfly@gmail.com>
2017-11-20 16:12:20 +0800

    StringSwitch: supports string_view type

--- lv/lv/StringSwitch.hpp
+++ lv/lv/StringSwitch.hpp
@@ -13,8 +13,9 @@
 
 #include <lv/Ensure.hpp>
 
-#include <boost/utility/string_view.hpp>
 #include <boost/assert.hpp>
+
+#include <string_view>
 
 
 // see llvm.StringSwitch
@@ -24,17 +25,18 @@ namespace lv
 	template<class T>
 	class StringSwitch
 	{
-		boost::string_view	str_;
+		std::string_view	str_;
 
 		T const *			result_;
 
 	public:
 
-		explicit StringSwitch(boost::string_view str)
+		explicit StringSwitch(std::string_view str)
 			: str_(str)
 			, result_(nullptr)
 		{
 		}
+
 
 		template<size_t N>
 		StringSwitch &	case_(char const (&s)[N], T const & value)
@@ -48,29 +50,40 @@ namespace lv
 			return *this;
 		}
 
-		template<size_t N>
-		StringSwitch &	starts_with(char const (&s)[N], T const & value)
+		StringSwitch &	case_(std::string_view s, T const & value)
 		{
-			if (result_ == nullptr && str_.size() >= N - 1 &&
-				std::memcmp(str_.data(), s, N - 1))
+			if (result_ == nullptr && str_ == s)
 			{
 				result_ = &value;
 			}
 
 			return *this;
 		}
+
+
+		template<size_t N>
+		StringSwitch &	starts_with(char const (&s)[N], T const & value)
+		{
+			return starts_with_impl(s, N - 1, value);
+		}
+
+		StringSwitch &	starts_with(std::string_view s, T const & value)
+		{
+			return starts_with_impl(s.data(), s.size(), value);
+		}
+
 
 		template<size_t N>
 		StringSwitch &	ends_with(char const (&s)[N], T const & value)
 		{
-			if (result_ == nullptr && str_.size() >= N - 1 &&
-				std::memcmp(str_.data() + str_.size() + 1 - N, s, N - 1) == 0)
-			{
-				result_ = &value;
-			}
-
-			return *this;
+			return ends_with_impl(s, N - 1, value);
 		}
+
+		StringSwitch &	ends_with(std::string_view s, T const & value)
+		{
+			return ends_with_impl(s.data(), s.size(), value);
+		}
+
 
 		template<class E>
 		StringSwitch &	except(E const & ex)
@@ -78,6 +91,7 @@ namespace lv
 			LV_ENSURE(result_ != nullptr, ex);
 			return *this;
 		}
+
 
 		T	default_(T const & value) const
 		{
@@ -89,10 +103,35 @@ namespace lv
 			return value;
 		}
 
+
 		operator T ()	const
 		{
 			BOOST_ASSERT(result_);
 			return *result_;
+		}
+
+	private:
+
+		StringSwitch &	starts_with_impl(char const * s, size_t size, T const & value)
+		{
+			if (result_ == nullptr && str_.size() >= size &&
+				std::memcmp(str_.data(), s, size) == 0)
+			{
+				result_ = &value;
+			}
+
+			return *this;
+		}
+
+		StringSwitch &	ends_with_impl(char const * s, size_t size, T const & value)
+		{
+			if (result_ == nullptr && str_.size() >= size &&
+				std::memcmp(str_.data() + str_.size() - size, s, size) == 0)
+			{
+				result_ = &value;
+			}
+
+			return *this;
 		}
 	};
 }
